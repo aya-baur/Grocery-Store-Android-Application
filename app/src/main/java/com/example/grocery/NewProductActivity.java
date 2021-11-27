0cff38059b558de35bfaff699f233b123b38c7f5
Aya Bauyrzhankyzy <aya.baur@gmail.com>
2021-11-26 19:44:50 -0500

    added class of new product page

---
+++ app/src/main/java/com/example/grocery/NewProductActivity.java
@@ -0,0 +1,18 @@
+package com.example.grocery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class NewProductActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_product_page);
+
+    }
+
+}
