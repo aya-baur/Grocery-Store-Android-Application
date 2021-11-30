e4cf00627571d6046544e3f2f5a8f6f80d07c872
Aya Bauyrzhankyzy <aya.baur@gmail.com>
2021-11-30 11:30:30 -0500

    linked the back button to ProductListActivity

--- app/src/main/java/com/example/grocery/NewProductActivity.java
+++ app/src/main/java/com/example/grocery/NewProductActivity.java
@@ -12,7 +12,6 @@ public class NewProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_product_page);
-
     }
 
 }
