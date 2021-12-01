c1c6073a6d8113b9c05c63029800a2f46715912b
YuehangGuo <suuupernigel@gmail.com>
2021-12-01 14:52:44 -0500

    functionality for product list(owner)

--- app/src/main/java/com/example/grocery/ProductListActivity.java
+++ app/src/main/java/com/example/grocery/ProductListActivity.java
@@ -6,18 +6,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ProductListActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     public static RecyclerView recyclerView;
-    public static String[] names, prices, quantities, unit;
+    public static FloatingActionButton add_new;
 
 
 
@@ -30,15 +40,45 @@ public class ProductListActivity extends AppCompatActivity {
 
 
         recyclerView = findViewById(R.id.product_list_recycler_view);
+        add_new = findViewById(R.id.floatingActionButton);
 
-        names = getResources().getStringArray(R.array.sample_products_names);
-        unit = getResources().getStringArray(R.array.sample_units);
-        prices = getResources().getStringArray(R.array.sample_prices);
-        quantities = getResources().getStringArray(R.array.sample_quantities);
 
-        ProductsListAdapter Adapter = new ProductsListAdapter(this, names, prices, quantities, unit);
+
+        ProductsListAdapter Adapter = new ProductsListAdapter(this, new ArrayList<>());
         recyclerView.setAdapter(Adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        String store_id = getIntent().getStringExtra("STORE_ID");
+
+
+
+        add_new.setOnClickListener((View view) -> {
+
+            Intent intent = new Intent(this, NewProductActivity.class);
+            intent.putExtra("STORE_ID", store_id);
+            this.startActivity(intent);
+        });
+    }
+
+    public void populateProductDataFromStoreId(String storeId, ProductsListAdapter adapter) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("stores").child(storeId);
+        ValueEventListener listener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Store store = dataSnapshot.getValue(Store.class);
+                ArrayList<Product> productList = new ArrayList<>(store.getProducts().values());
+                Log.i("orders change", dataSnapshot.toString());
+                adapter.setProducts(productList);
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("warning", "loadPost:onCancelled",
+                        databaseError.toException());
+            }
+        };
+        ref.addListenerForSingleValueEvent(listener);
+
     }
 
 }
--- app/src/main/java/com/example/grocery/ProductsListAdapter.java
+++ app/src/main/java/com/example/grocery/ProductsListAdapter.java
@@ -16,34 +16,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 public class ProductsListAdapter extends RecyclerView.Adapter<ProductsListAdapter.ViewHolder> {
-    String[] names_arr, price_arr,quantity_arr,  units_arr;
+    ArrayList<Product> Products;
     Context context;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView testView1, testView2, testView3, testView4;
-        EditText editText;
+        TextView testView1, testView2, testView4;
         Button button;
 
         public ViewHolder(View view) {
             super(view);
             testView1 = view.findViewById(R.id.textViewName);
             testView2 = view.findViewById(R.id.textViewPrice);
-            testView3 = view.findViewById(R.id.textViewQuantity);
             testView4 = view.findViewById(R.id.textViewUnit);
-            editText = view.findViewById(R.id.editTextNumberSigned);
-            button = view.findViewById(R.id.button_update);
         }
     }
 
-    public ProductsListAdapter(Context context, String[] s1, String[] s2, String[] s3, String[] s4) {
+    public ProductsListAdapter(Context context, ArrayList<Product> Products) {
         this.context = context;
-        names_arr = s1;
-        price_arr = s2;
-        quantity_arr = s3;
-        units_arr = s4;
+        this.Products = Products;
 
 
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.Products = products;
+        this.notifyItemInserted(Products.size());
     }
 
     @NonNull
@@ -56,36 +56,15 @@ public class ProductsListAdapter extends RecyclerView.Adapter<ProductsListAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.testView1.setText(names_arr[position]);
-        holder.testView2.setText(price_arr[position]);
-        holder.testView3.setText(quantity_arr[position]);
-        holder.testView4.setText(units_arr[position]);
-        holder.button.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-            if (quantity_arr[position] != null && !holder.editText.getText().toString().equals("")){
-                int newQuantity= Integer.valueOf(quantity_arr[position]) + Integer.valueOf(holder.editText.getText().toString());
-                if(newQuantity >= 0){
-                    quantity_arr[position] = String.valueOf(newQuantity);
-                    holder.testView3.setText(quantity_arr[position]);
-                    holder.editText.getText().clear();}
+        holder.testView1.setText(Products.get(position).getName());
+        holder.testView2.setText(String.valueOf(Products.get(position).getPrice()));
+        holder.testView4.setText(Products.get(position).getUnit());
 
-                else{Toast toast = Toast.makeText(view.getContext(), R.string.app_name, Toast.LENGTH_SHORT);
-                    toast.show();}
-
-
-
-
-
-                }
-            }
-        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return names_arr.length;
+        return Products.size();
     }
 }
